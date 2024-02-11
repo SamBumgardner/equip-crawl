5da1ffbe3befd59cfe97b3a2e23662d97b54ad0b
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-02-11 13:27:05 -0600

    basic game-ending handling

--- combat/action/examples/Action_SpearThrust.gd
+++ combat/action/examples/Action_SpearThrust.gd
@@ -3,16 +3,16 @@ class_name Action_SpearThrust extends Action
 static var on_act_actions:Array[CombatActionEffect]
 
 func _init():
-    name = "Spear Thrust"
-    charge_time = 2
-    recovery_time = 1
-    
-    if (on_act_actions == null):
-        var range1 = EffectiveRange.new(Position.Ranges.SHORT, Position.Ranges.SHORT, 
-            EffectiveRange.RangeDirections.FRONT)
-        var damage_effect = DamageEffect.new(CombatActionEffect.Target.OTHER, range1, 5)
+	name = "Spear Thrust"
+	charge_time = 2
+	recovery_time = 1
+	
+	if (on_act_actions.is_empty()):
+		var range1 = EffectiveRange.new(Position.Ranges.SHORT, Position.Ranges.SHORT, 
+			EffectiveRange.RangeDirections.FRONT)
+		var damage_effect = DamageEffect.new(CombatActionEffect.Target.OTHER, range1, 5)
 
-        on_act_actions = [damage_effect] 
+		on_act_actions = [damage_effect] 
 
 func on_act() -> Array[CombatActionEffect]:
-    return on_act_actions
+	return on_act_actions
--- combat/actor/Combatant.gd
+++ combat/actor/Combatant.gd
@@ -1,6 +1,17 @@
 class_name Combatant extends Node
 
-@export var health : float
+signal combatant_defeated(combatant)
+signal health_changed(new_value)
+
+@export var health : float:
+	get:
+		return health
+	set(value):
+		health = value
+		health_changed.emit(health)
+		if value <= 0:
+			combatant_defeated.emit(self)
+
 @export var target_other : Combatant
 
 var state_machine : StateMachine 
@@ -18,7 +29,7 @@ func _physics_process(delta):
 
 func send_current_act_effects():
 	var combat_effects = current_action.on_act()
-	# run effects through ongoing statuses, then send em to their target
+	#TODO: run effects through ongoing statuses, then send em to their target
 	var target_self_effects : Array[CombatActionEffect] = []
 	var target_other_effects : Array[CombatActionEffect] = []
 
@@ -34,5 +45,23 @@ func send_current_act_effects():
 		target_other.receive_act_effects(target_other_effects)
 
 func receive_act_effects(received_effects: Array[CombatActionEffect]):
-	print("Received action effects: ", received_effects)
-	pass # todo: take in list of stuff to do
+	print("owner ", self, " received action effects: ", received_effects)
+	#TODO: run effects through ongoing statuses, then accept the results
+	_apply_received_effects(received_effects)
+
+func _apply_received_effects(received_effects : Array[CombatActionEffect]):
+	for combat_effect in received_effects:
+		match combat_effect.type:
+			CombatActionEffect.EffectType.DAMAGE:
+				var incoming_damage = (combat_effect as DamageEffect).amount
+				health -= incoming_damage
+				print("owner ", self, "ouch, just took ", incoming_damage, " damage, only ", health, " health remaining!")
+			CombatActionEffect.EffectType.HEAL:
+				var incoming_heal = (combat_effect as HealEffect).amount
+				health += incoming_heal
+				print("owner ", self, "whew, just got ", incoming_heal, " healing, I have ", health, " health remaining!")
+			CombatActionEffect.EffectType.STATUS:
+				pass
+			CombatActionEffect.EffectType.MOVE:
+				#TODO: call overridden function for handling movement effects(?)
+				pass
