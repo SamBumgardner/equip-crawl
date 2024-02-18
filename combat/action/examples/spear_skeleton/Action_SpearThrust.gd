e13f83ba9dce6aa74a9eb0af3e974d098a7887b5
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-02-18 12:51:14 -0600

    basic version of enemy logic tree

---
+++ combat/action/examples/spear_skeleton/Action_SpearThrust.gd
@@ -0,0 +1,22 @@
+class_name Action_SpearThrust extends Action
+
+static var on_act_actions:Array[CombatActionEffect]
+
+static var last_turn_direction = -1
+
+func _init(owning_enemy : Enemy):
+	enemy = owning_enemy
+	
+	name = "Spear Thrust"
+	charge_time = 2
+	recovery_time = 1
+	
+	if (on_act_actions.is_empty()):
+		var range1 = EffectiveRange.new(Position.Ranges.SHORT, Position.Ranges.MEDIUM, 
+			EffectiveRange.RangeDirections.FRONT)
+		var damage_effect = DamageEffect.new(CombatActionEffect.Target.OTHER, range1, 5)
+
+		on_act_actions = [damage_effect] 
+
+func on_act() -> Array[CombatActionEffect]:
+	return on_act_actions
--- combat/actor/states/enemy/EnemyIdle.gd
+++ combat/actor/states/enemy/EnemyIdle.gd
@@ -3,6 +3,14 @@ class_name EnemyIdle extends ActorState
 func physics_process(delta : float) -> StateChange:
 	# todo: use logic tree here to decide if a new action should begin.
 	# for now, will just make them immediately start charging
+	
+	if (owner as Enemy).get_turn_direction_toward_player() != MoveEffect.LateralDirection.NONE:
+		owner.current_action = Action_Turn.new(owner)
+	elif (owner as Enemy).get_distance_to_player() != Position.Ranges.MEDIUM and !(owner.current_action is Action_MidrangeMove):
+		owner.current_action = Action_MidrangeMove.new(owner)
+	else:
+		owner.current_action = Action_SpearThrust.new(owner)
+	
 	state_change.next_state = CombatantStates.States.CHARGE
 	state_change.remaining_delta = delta
 	return state_change 
