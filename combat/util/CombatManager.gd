eb11b385dd28efb4e83c18438e01239643ee2bec
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-02-23 22:29:34 -0600

    added notes to prep for combat finished

--- combat/util/CombatManager.gd
+++ combat/util/CombatManager.gd
@@ -12,10 +12,13 @@ func _on_combatant_defeated(defeated : Combatant):
 	print("\nGAMEPLAY MANAGER ANNOUNCEMENT:\n", defeated, " was defeated! Starting end sequence now\n")
 	player.process_mode = Node.PROCESS_MODE_DISABLED
 	enemy.process_mode = Node.PROCESS_MODE_DISABLED
+	#todo: emit signal saying let's begin game-over-ing
+	# For this version, we can just have a rule that all things finish their 
+	# game over sequence in 3 seconds. This just assumes that everything's 
+	# already been triggered by the thing that became defeated (sent visual events and whatever).
 	$EndCombatSequenceTimer.start()
 
 func _on_combat_end_sequence_finished():
 	print("\nGAMEPLAY MANAGER ANNOUNCEMENT:\nend sequence finished. Cleaning up nodes")
-	player.queue_free()
-	enemy.queue_free()
-	self.queue_free()
+	#todo: push event to Combat, Combat pushes out event to the void saying it's all done now.
+	# Combat then just waits, transitioning thing can handle queue_free ing it.
